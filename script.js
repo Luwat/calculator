c9a4c28a9514b57d4afb2df6e31e9dcba5d8a09b
luwat <oluwatobioreniyi@gmail.com>
2022-07-01 08:20:04 +0100

    basic maths operators

---
+++ script.js
@@ -0,0 +1,9 @@
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const num1 = parseInt(prompt('insert number', 0));
+const num2 = parseInt(prompt('insert number', 0));
+
+console.log(subtract(num1, num2));
