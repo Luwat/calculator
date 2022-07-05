ee1184242bf57f573b88267963ba5091ca28100f
luwat <oluwatobioreniyi@gmail.com>
2022-07-05 06:35:58 +0100

    function operate done

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-const num1 = parseInt(prompt('insert number', 0));
-const num2 = parseInt(prompt('insert number', 0));
 
-console.log(subtract(num1, num2));
+
+
+const operate = () => {
+    const num1 = parseInt(prompt('insert number', 0));
+    const operator = prompt('insert operator',);
+    const num2 = parseInt(prompt('insert number', 0));
+    
+    if(operator === '+') return add(num1, num2);
+    if(operator === '-') return subtract(num1, num2);
+    if(operator === '*') return multiply(num1, num2);
+    if(operator === '/') return divide(num1, num2);
+}
+
+console.log(operate())
