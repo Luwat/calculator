049d72744ec476432ae13bd5fce372aeb4f0340f
luwat <oluwatobioreniyi@gmail.com>
2022-07-30 11:07:15 +0100

    trying a different approach as equals isn't working

--- script.js
+++ script.js
@@ -1,68 +1,26 @@
-const chooseOperator  = (operator) => {
-    if (currentOperandDisplay.innerText === '' ||
-    currentOperandDisplay.innerText.includes(operator)){
-            return
-    }
-    currentOperandDisplay.textContent += ` ${operator} `
-    previousOperandDisplay.innerText = currentOperandDisplay.innerText;
-    currentOperandDisplay.innerText = ''
-};
-
-const chooseNumber  = (number) => {
-    if (currentOperandDisplay.innerText.indexOf(".") === -1)
-    currentOperandDisplay.textContent += number
-};
-
-const del = () => {
-    return currentOperandDisplay.innerText = currentOperandDisplay.innerText.slice(0,currentOperandDisplay.innerText.length -1);
-}
-
-const clear = () => {
-    currentOperandDisplay.innerText = ''
-    previousOperandDisplay.innerText = ''
-}
-
-
-
-
-const operate = (operator) => {
-    const prev = parseInt(previousOperandDisplay.innerText);
-    const current = parseFloat(currentOperandDisplay.innerText);
-    if (operator === '+') {
-        return prev + current;
-    } else if (operator === '-'){
-        return prev - current;
-    } else if (operator === '*'){
-        return console.log(prev * current);
-    } else if (operator === '/'){
-        return console.log(prev / current);
-    }
-};
-
-
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const clearButton = document.querySelector('[data-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-const equalsButton = document.querySelector('[data-equals]');
 const previousOperandDisplay = document.querySelector('[data-previous-operand]')
 const currentOperandDisplay = document.querySelector('[data-current-operand]')
+const buttons = document.querySelectorAll('button');
 
 
-numberButtons.forEach(button => {
+buttons.forEach(button => {
     button.addEventListener('click', () => {
-        chooseNumber(button.innerText)
+        if (button.classList.contains('number')) {
+            if (currentOperandDisplay.innerText.includes('.') &&
+                    button.innerText === '.') {
+                return
+            }
+            currentOperandDisplay.textContent += button.innerText;
+        } else if (button.classList.contains('operation')){
+            currentOperandDisplay.textContent += button.innerText;
+        } else if (button.classList.contains('clear')) {
+            currentOperandDisplay.textContent = '';
+        } else if (button.classList.contains('delete')) {
+            currentOperandDisplay.innerText = 
+            currentOperandDisplay.innerText.slice(0, -1);
+        } else if (button.classList.contains('equals')) {
+            currentOperandDisplay.textContent = 
+        }
     })
 })
 
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        chooseOperator(button.innerText);
-    })
-})
-
-deleteButton.addEventListener('click', del)
-
-clearButton.addEventListener('click', clear)
-
-equalsButton.addEventListener('click', operate)
