09549819450065ee87e99c5c637ef33826e51798
luwat <oluwatobioreniyi@gmail.com>
2022-08-01 14:15:01 +0100

    done ofr now

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        let operation = button.classList.contains('operation')
         if (button.classList.contains('number')) {
             if (currentOperandDisplay.innerText.includes('.') &&
                     button.innerText === '.') {
@@ -12,15 +13,32 @@ buttons.forEach(button => {
             }
             currentOperandDisplay.textContent += button.innerText;
         } else if (button.classList.contains('operation')){
+            if (currentOperandDisplay.innerText.includes(operation)) {
+                return
+            }
             currentOperandDisplay.textContent += button.innerText;
+
         } else if (button.classList.contains('clear')) {
             currentOperandDisplay.textContent = '';
         } else if (button.classList.contains('delete')) {
             currentOperandDisplay.innerText = 
             currentOperandDisplay.innerText.slice(0, -1);
-        } else if (button.classList.contains('equals')) {
-            currentOperandDisplay.textContent = 
+        } else if (button.classList.contains('equals') && button.innerText === '=') {
+            currentOperandDisplay.textContent = eval(currentOperandDisplay.innerText)
         }
     })
 })
 
+const operate = (x, y, op) => {
+    if (op === '+') {
+        return x + y
+    } else if (op === '-') {
+        return x - y
+    } else if (op === '*') {
+        return x * y
+    } else if (op === '÷') {
+        return x / y
+    } else {
+        return
+    }
+}
